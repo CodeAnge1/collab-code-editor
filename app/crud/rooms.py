ed6ee0f2b6f6e2b6bc55e5333544c406cb56acb1
Maksim Mikhailov <87376297+CodeAnge1@users.noreply.github.com>
2025-12-14 01:01:03 +0300

    feat: update CRUD to new model fields

--- app/crud/rooms.py
+++ app/crud/rooms.py
@@ -1,15 +1,29 @@
 from typing import List
+from uuid import UUID
 
-from sqlalchemy import and_
-from sqlalchemy.orm import joinedload, Session
+from sqlalchemy.orm import selectinload, Session
 
 from app.crud.users import get_user_by_id
 from app.models.rooms import Room, RoomUser
+from app.schemas.rooms import RoomCreate, RoomType
+from app.utils import get_password_hash, verify_password
 
 
-def create_room(db: Session, name: str, owner_id: int) -> Room | None:
+def user_in_room(db: Session, room_id: str, user_id: int) -> bool:
+	return db.query(RoomUser).filter(
+		RoomUser.room_id == UUID(room_id),
+		RoomUser.user_id == user_id
+	).first() is not None
+
+
+def create_room(db: Session, room: RoomCreate, owner_id: int) -> Room | None:
 	if get_user_by_id(db, owner_id):
-		new_room = Room(name=name, owner_id=owner_id)
+		new_room = Room(
+			name=room.name,
+			owner_id=owner_id,
+			type=room.type,
+			hashed_password=get_password_hash(room.password) if room.type == RoomType.PRIVATE else None
+		)
 		room_user = RoomUser(user_id=owner_id, role="owner")
 		new_room.users.append(room_user)
 		db.add(new_room)
@@ -19,33 +33,35 @@ def create_room(db: Session, name: str, owner_id: int) -> Room | None:
 	return None
 
 
-def get_room(db: Session, room_id: int) -> Room | None:
-	return db.query(Room).filter(Room.id == room_id).first()
-
-
-def add_user_to_room(db: Session, room_id: int, user_id: int, role: str = "viewer") -> Room | None:
-	room = get_room(db, room_id)
-	if room:
-		user_in_room = db.query(RoomUser).filter(
-			and_(
-				RoomUser.room_id == room_id,
-				RoomUser.user_id == user_id
-			)
-		).first()
-		if not user_in_room:
-			room_user = RoomUser(
-				user_id=user_id,
-				role=role
-			)
-			room.users.append(room_user)
-			db.commit()
-			db.refresh(room)
+def get_room(db: Session, room_id: str, user_id: int) -> Room | None:
+	room = db.query(Room).filter(Room.id == UUID(room_id)).options(selectinload(Room.users)).first()
+	if room.type == RoomType.PUBLIC:
 		return room
-	return None
+	return room if room and any(user.user_id == user_id for user in room.users) else None
 
 
-def delete_room(db: Session, room_id: int) -> bool:
-	room = get_room(db, room_id)
+def add_user_to_room(db: Session, room_id: str, user_id: int, password: str | None = None,
+					 role: str = "viewer") -> Room | None:
+	room = get_room(db, room_id, user_id)
+	if not room:
+		return None
+	if user_in_room(db, room_id, user_id):
+		return room
+	if room.type == RoomType.PRIVATE:
+		if not password or not verify_password(password, room.hashed_password):
+			return None
+	room_user = RoomUser(
+		user_id=user_id,
+		role=role
+	)
+	room.users.append(room_user)
+	db.commit()
+	db.refresh(room)
+	return room
+
+
+def delete_room(db: Session, room_id: str, user_id: int) -> bool:
+	room = db.query(Room).filter(Room.id == UUID(room_id), Room.owner_id == user_id).first()
 	if room:
 		db.delete(room)
 		db.commit()
@@ -58,7 +74,6 @@ def get_user_rooms(db: Session, user_id: int) -> List[Room]:
 		db.query(Room)
 		.join(RoomUser, Room.id == RoomUser.room_id)
 		.filter(RoomUser.user_id == user_id)
-		.options(joinedload(Room.users))
 		.all()
 	)
 	return user_rooms
